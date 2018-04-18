a098ef1cd03b0f5df566f7992250f06b82fbd15d
Autumn <autumn2@sting.graceland.edu>
2018-04-18 12:16:31 -0500

    javascript

---
+++ JavaScript1/info.js
@@ -0,0 +1,14 @@
+
+var name = "Autumn Franke";
+var date = new Date();
+document.write("<span id ='footer'> Autumn's Website</span><br />");
+
+document.write("Basic Website");
+document.write("<br>");
+document.write("2018 Autumn Franke");
+document.write("<br>");
+document.write("April 18 2018");
+document.write("<br>");
+document.write(Date());
+
+
